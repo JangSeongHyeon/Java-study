5872d03966877e851078061c611d13c567ce37e6
JangSeongHyeon <dwg04045@gmail.com>
2021-10-03 11:51:44 +0900

    study of ch05

---
+++ ch05/src/Ex5_1_tmp.java
@@ -0,0 +1,13 @@
+
+public class Ex5_1_tmp {
+
+	public static void main(String[] args) {
+		// index범위 0~9
+		int[] arr=new int[8];		// 길이가 5인 int 배열 arr을 생성
+		System.out.println("arr.length="+arr.length);
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("arr["+i+"]="+arr[i]);
+		}
+	}
+}
